fafdbbb78152513d5dcd9ef8027a76c03dd33b89
surranc20 <surranc20@mail.wlu.edu>
2022-04-10 16:04:21 -0500

    Refactor Loading

--- src/scenes/playing.ts
+++ src/scenes/playing.ts
@@ -1,4 +1,6 @@
-import { Container } from "pixi.js";
+import { Container, Loader } from "pixi.js";
+import Pacman from "../game_objects/pacman";
+import Stage from "../game_objects/stage";
 import IDrawable from "../interfaces/iDrawable";
 import IScene from "../interfaces/iScene";
 import MazeModel from "../models/mazeModel";
@@ -6,6 +8,7 @@ import MazeModel from "../models/mazeModel";
 export default class Playing implements IScene {
   stage = new Container();
   mazeModel: MazeModel;
+  pacman: any;
 
   constructor() {
     this.mazeModel = new MazeModel();
@@ -16,5 +19,22 @@ export default class Playing implements IScene {
       let gameObject = child as IDrawable;
       gameObject.update(elapsedTime);
     }
+  }
+
+  addAssetsToLoader(loader: Loader) {
+    loader.add("pacman_eat", "/assets/img/pacman/pacman_eat.json");
+    loader.add("stage", "/assets/img/stage.png");
+  }
+
+  onDoneLoading(resources: any) {
+    // Create Stage
+    const stage = new Stage(0, 24, resources.stage.texture);
+    this.stage.addChild(stage);
+
+    // Create Pacman
+    const sheet = resources.pacman_eat?.spritesheet;
+    this.pacman = new Pacman(sheet?.animations.pacman_eat, 108, 205);
+
+    this.stage.addChild(this.pacman);
   }
 }
--- src/utils/gameManager.ts
+++ src/utils/gameManager.ts
@@ -1,13 +1,10 @@
-import * as PIXI from "pixi.js";
-import { AbstractRenderer } from "pixi.js";
+import { AbstractRenderer, Loader } from "pixi.js";
 import GameTicker from "./gameTicker";
-import Pacman from "../game_objects/pacman";
 import Playing from "../scenes/playing";
 import IScene from "../interfaces/iScene";
 
 export default class GameManager {
   scene: IScene;
-  pacman!: Pacman;
   gameTicker: GameTicker;
 
   constructor(renderer: AbstractRenderer) {
@@ -23,14 +20,11 @@ export default class GameManager {
   };
 
   loadGame() {
-    const loader = PIXI.Loader.shared;
-    loader.add("pacman_eat", "/assets/img/pacman/pacman_right.json");
+    const loader = Loader.shared;
+    this.scene.addAssetsToLoader(loader);
 
     loader.load((_, resources) => {
-      const sheet = resources.pacman_eat?.spritesheet;
-      this.pacman = new Pacman(sheet?.animations.pacman_right, 50, 50);
-      this.scene.stage.addChild(this.pacman);
-
+      this.scene.onDoneLoading(resources);
       this.gameTicker.startTicking();
     });
   }
