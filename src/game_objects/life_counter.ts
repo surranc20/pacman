232ca1ffe5a1461c106f60bbed891091eaafeb7a
surranc20 <surranc20@mail.wlu.edu>
2022-05-15 22:49:09 -0500

    Pacman Death Animation and Reset Level

--- src/game_objects/life_counter.ts
+++ src/game_objects/life_counter.ts
@@ -3,20 +3,23 @@ import Drawable from "../abstract/drawable";
 
 export default class LifeCounter {
   container: Container;
+  lives: number;
 
   constructor(lives: number) {
     this.container = new Container();
+    this.lives = lives;
     this.setLives(lives);
   }
 
   setLives(lives: number) {
+    this.lives = lives;
     const lifeTexture =
       Loader.shared.resources.spritesheet.spritesheet?.textures[
         "pacman_life.png"
       ];
     this.container.removeChildren();
     if (lifeTexture) {
-      for (let x = 0; x < lives - 1; x++) {
+      for (let x = 0; x < this.lives - 1; x++) {
         this.container.addChild(new PacmanLifeDrawable(x * 16, 0, lifeTexture));
       }
     }
